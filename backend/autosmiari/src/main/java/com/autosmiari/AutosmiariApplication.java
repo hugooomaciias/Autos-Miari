888d89d6e816948e7b71ea6b94630be6b1549d6e
fernando <Fog-03@hotmail.com>
2025-04-29 21:04:00 +0200

    Añadiendo algo de backend

---
+++ backend/autosmiari/src/main/java/com/autosmiari/AutosmiariApplication.java
@@ -0,0 +1,13 @@
+package com.autosmiari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosmiariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosmiariApplication.class, args);
+	}
+
+}
---
+++ backend/autosmiari/src/test/java/com/autosmiari/AutosmiariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autosmiari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosmiariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
